4689f777eae4992fe00ccc04a48ecf197ebc2576
pantless wonder <fake@email.com>
2012-11-12 18:02:10 -0500

    jump into REPL and some other nice things

--- client.js
+++ client.js
@@ -3,7 +3,9 @@ var cookie = '';
 
 //grab the important libraries that we'll need
 var http = require('http'),
-    io = require('socket.io-client');
+    io = require('socket.io-client'),
+	repl = require('repl'),
+	r = null;
 
 //Override the built in http class to always send our cookie, regardless of
 //where we'll be sending to. Sure, this violates spec, but in my defense we
@@ -18,27 +20,58 @@ http.OutgoingMessage.prototype._renderHeaders = function() {
 	
 	return this.__renderHeaders();
 }
+
+function postHelper(host, path, json, callback) {
+	var post_options = {
+		host: host,
+		port: '80',
+		path: path,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': json.length,
+			'Cookie': cookie
+		}};
+
+	// Set up the request
+	var post_req = http.request(post_options, function(res) {
+		res.setEncoding('utf8');
+		var chunks = [];
+		res.on('data', function (chunk) {
+			chunks.push(chunk);
+		});
+
+		res.on('end', function(){
+			callback ? callback(chunks.join('')) : null;
+		});
+	});
+
+  // post the data
+  post_req.write(json);
+  post_req.end();
+}
   
 //web socket event handlers
 var handlers = {
 	'chat': function(data){
-		console.log('<' + data.from + '> ' + data.message);
-		
-		if ('herp' === data.message)
+		if (!r || !r.context.mute)
 		{
-			sock.emit('chat', 'derp');
+			console.log('<' + data.from + '> ' + data.message);
 		}
 	},
 	'userLeave': function(data){
 		console.log(data.username + ' left.');
 	},
 	'userJoin': function(data){
-		console.log(data.username + ' joined.');
+		console.log(data.user.username + ' joined.');
 	},
 	'ping': function(data){
-		//rpcGW.execute('channel.pong', null);
-		//I'm pretty sure this is bad if we don't respond here
-	}
+		console.log('Responding to ping!');
+		postHelper('www.plug.dj', '/gateway/channel.pong', '{"service":"channel.pong","body":[]}', function(data){
+			console.log('pong response: ', data);
+		});
+	},
+	'voteUpdate': function(){}
 };
 
 //connect and listen for the various important events
@@ -46,10 +79,19 @@ var sock = io.connect('s.plug.dj:843');
 sock.on('connecting', function (transport_type) {
 	console.log('connecting via ' + transport_type);
 });
-sock.on('connect', function(){
-	this.emit('join', 'coding-soundtrack');
-	this.emit('set language', 'en');
+sock.on('connect', function(realsocket){
+	sock.emit('join', 'coding-soundtrack');
+	sock.emit('set language', 'en');
 	console.log('Connected!!!');
+	
+	postHelper('www.plug.dj', '/gateway/room.join', '{"service":"room.join","body":["coding-soundtrack"]}', function(data){
+		console.log('room-join got: ' + data.length + 'b back');
+	});
+	
+	r = repl.start("node> ");
+	r.context.socket = sock;
+	r.context.handlers = handlers;
+	r.context.mute = false;
 });
 sock.on('connect_failed', function () {
 	console.log('connect failed ...');
